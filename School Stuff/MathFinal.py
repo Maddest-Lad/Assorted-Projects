d2b848c8f25125f9e1b5258c13342536648e0b22
SamHarris2020 <sam.career@outlook.com>
2020-05-31 22:57:28 -0700

    Fixed Some Formating, Added Pillow to requirements.txt

--- School Stuff/MathFinal.py
+++ School Stuff/MathFinal.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 
 total = 0
-loop_count = 0
 
 salary = 67500
 bonus = 1500
@@ -15,12 +14,11 @@ for i in range(15):
 
     y.append(total)
 
-
 plt.plot(x, y)
 
 plt.xlabel('years')
 plt.ylabel('Millions earned')
 
-plt.title('Mayas Earnings Over ~14 Years')
+plt.title('Mayas Earnings Over 14 Years')
 
 plt.show()
