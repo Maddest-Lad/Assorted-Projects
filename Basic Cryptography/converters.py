9687ac52f0352eb3e45460a8c14b80d43243976f
SamHarris2020 <sam.career@outlook.com>
2020-06-08 20:40:59 -0700

    Did Stuff

--- Basic Cryptography/converters.py
+++ Basic Cryptography/converters.py
@@ -1,3 +1,19 @@
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+ALPHANUMERICS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 
+# Returns All 25 Cesar Cipher Shifts
+def cesar_cipher(text: str) -> [str]:
+    for key in range(len(LETTERS)):
+        translated = ''
+        for symbol in text:
+            if symbol in LETTERS:
+                num = LETTERS.find(symbol)
+                num = num - key
+                if num < 0:
+                    num = num + len(LETTERS)
+                translated = translated + LETTERS[num]
+            else:
+                translated = translated + symbol
+        print('Hacking key #%s: %s' % (key, translated))
 
-def int_to_hex():
+cesar_cipher("Test")
