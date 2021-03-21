f93f2d0c41af058c6e6c6a2b16589ba3984ca661
SamHarris2020 <sam.career@outlook.com>
2021-03-20 22:17:12 -0400

    Using Data Extracted From Discord, Print Out Top Message Senders

---
+++ Discord Message Totals/Summarize.py
@@ -0,0 +1,46 @@
+import csv
+import os
+from collections import Counter
+
+# Stat to Track
+user_messages = Counter()
+user_words = Counter()
+
+# Pull Up The CSV Folder the Lazy Way
+os.chdir("Data")
+
+# Pick Directory
+for file_name in os.listdir(os.getcwd()):
+
+    # Open Files
+    with open(file_name, encoding="utf_8") as csv_file:
+
+        reader = csv.reader(csv_file, delimiter=',')
+
+        # For Rows in the CSV (Author ID, Author, Date, Content, Attachments, Reactions)
+
+        try:
+
+            for row in reader:
+                user_messages[row[1]] += 1 # Message Count
+                user_words[row[1]] += len(row[3].split()) # Word Count
+        except:
+            continue
+
+    # By God Close The File Before Memory Dies
+    csv_file.close()
+
+new_list = Counter()
+
+for i in user_words.most_common():
+
+    try:
+        new_list[i[0]] = i[1] / user_messages[i[0]]
+
+    except:
+
+        continue
+
+# Nice Aligned Print
+for x in new_list.most_common():
+    print("%-*s Avg: %s" % (30, x[0], round(x[1], 2)))
