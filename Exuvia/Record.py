97a37ae6fe9c052829ff69a7b131145b024562c4
SamHarris2020 <sam.career@outlook.com>
2021-04-13 22:40:14 -0400

    Update

--- Exuvia/Record.py
+++ Exuvia/Record.py
@@ -1,43 +1,45 @@
 import wave
-
+import os
+import time
 import pyaudio
 
-chunk = 1024  # Record In Chunks of 1024 Samples
-sample_format = pyaudio.paInt16  # 16 bits per sample
-channels = 2
-fs = 44100  # Record at 44100 samples per second
-seconds = 3
-filename = "output.wav"
+CHUNK = 1024  # Record In Chunks of 1024 Samples
+SAMPLE_FORMAT = pyaudio.paInt16  # 16 bits per sample
+CHANNELS = 2
+FS = 44100  # Record at 44100 samples per second
+SECONDS = 3
+FILENAME = "output.wav"
 
-p = pyaudio.PyAudio()  # Create an interface to PortAudio
 
-print('Recording')
+def record(length: int):
+    # Create an interface to PortAudio
+    p = pyaudio.PyAudio()
 
-stream = p.open(format=sample_format,
-                channels=channels,
-                rate=fs,
-                frames_per_buffer=chunk,
-                input=True)
+    stream = p.open(format=SAMPLE_FORMAT, channels=CHANNELS, rate=FS, frames_per_buffer=CHUNK, input=True)
 
-frames = []  # Initialize array to store frames
+    frames = []  # Initialize array to store frames
 
-# Store data in chunks for 3 seconds
-for i in range(0, int(fs / chunk * seconds)):
-    data = stream.read(chunk)
-    frames.append(data)
+    # Store data in chunks for 3 seconds
+    for i in range(0, int(FS / CHUNK * SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
 
-# Stop and close the stream
-stream.stop_stream()
-stream.close()
-# Terminate the PortAudio interface
-p.terminate()
+    # Stop and close the stream
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
 
-print('Finished recording')
+    # Only Keep So Much Data
+    files = [file for file in os.listdir(os.path.join(os.getcwd(), "data"))]
+    files.sort(key=os.path.getmtime)
 
-# Save the recorded data as a WAV file
-wf = wave.open(filename, 'wb')
-wf.setnchannels(channels)
-wf.setsampwidth(p.get_sample_size(sample_format))
-wf.setframerate(fs)
-wf.writeframes(b''.join(frames))
-wf.close()
+    print(files)
+
+    # Save the recorded data as a WAV file
+    wf = wave.open("output.wav", 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(SAMPLE_FORMAT))
+    wf.setframerate(FS)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
